e1143c796ff0a2fb172a3a02ed4f9b00c6967192
msk <miso6619@gmail.com>
2026-04-29 07:09:21 +0900

    fix menu path script

--- js/ngo.js
+++ js/ngo.js
@@ -1,5 +1,20 @@
 function initMenu() {
   //console.log("initMenu 실행됨");
+  const isSubPage = location.pathname.includes("/page/");
+
+  const paths = {
+    home: isSubPage ? "../index.html" : "index.html",
+    headsets: isSubPage ? "./headsets.html" : "page/headsets.html",
+    speakers: isSubPage ? "./speakers.html" : "page/speakers.html",
+    about: isSubPage ? "./aboutBrand.html" : "page/aboutBrand.html",
+    qna: isSubPage ? "./qna.html" : "page/qna.html",
+  };
+
+  $("[data-link]").each(function () {
+    const key = $(this).data("link");
+    $(this).attr("href", paths[key]);
+  });
+
 
   $(".menu-open").off("click").on("click", function () {
     //console.log("메뉴 열기 클릭");
